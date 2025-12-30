294dca8253adc825a7b91dfd55874a4de234e876
Devu2004 <devansh7595@gmail.com>
2025-12-31 00:12:26 +0530

    backend deployed!

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://nova-ai-backend-gp53.onrender.com/api', // Tera Backend URL
+    baseURL: 'https://nova-ai-backend-ixnj.onrender.com/api', // Tera Backend URL
     withCredentials: true, // Cookies handle karne ke liye (Sabse Important)
 });
 
